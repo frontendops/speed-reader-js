d6102cae4cf57d9332f599a8448f37ef1301ccda
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-04-05 23:18:47 -0400

    fixed empty spaces in paragraph

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const text = document.querySelector('.words-container')
 const playBtn = document.querySelector('.play-dark')
 
-let words = text.innerHTML.trim().split(" ")
+let words = text.innerHTML.trim().split(" ").filter(word => word !== "");
 console.log(words)
+
 let index = -1
-
-
-// function playWords(words) {
-//     let index = 0
-//     setTimeout(() => {
-//         console.log(words[index])
-//         index++
-//         console.log(index)
-//         // text.innerHTML = ""
-//         // text.innerHTML = word
-//     }, 1000);
-
-// }
 
 let interval = null
 
@@ -25,24 +13,12 @@ function startTimer() {
     text.innerHTML = ""
     text.innerHTML = `<span class="single-word">${words[index]}</span>`
     console.log(words[index])
-    time = setTimeout(startTimer, 300);
+    time = setTimeout(startTimer, 250);
 
     if (index === words.length - 1) {
         clearTimeout(time);
     }
 }
-
-// function playWords(words) {
-//     console.log(words[index]);
-//     index++
-//     //stopWords()
-// }
-
-// function stopWords() {
-//     if (index === words.length) {
-//         clearInterval(playWords)
-//     }
-// }
 
 playBtn.addEventListener('click', () => {
     startTimer()
